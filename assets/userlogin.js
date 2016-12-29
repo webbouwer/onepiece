f5de4e457e2c4f5015adfe1a174145d3e47b0350
Oddsized <oddsized@gmail.com>
2016-12-29 21:30:03 +0100

    Loginbox toggle click slide up when down

--- assets/userlogin.js
+++ assets/userlogin.js
@@ -6,7 +6,11 @@ jQuery(function($) {
 
 		$('ul.tabmenu li,div.resetlogin').on('click', function(){
 															   
-															   
+			if($(this).hasClass('active')){
+				$(this).removeClass('active');
+				$('li.tab').slideUp();
+			}else{
+
 			$('ul.tabmenu li,div.resetlogin ').removeClass('active');
             $(this).addClass('active');
 			$('li.tab').slideUp();
@@ -14,6 +18,8 @@ jQuery(function($) {
     			$("ul.tabcontainer li").eq($(3).index()).slideDown();
 			}else{
     			$("ul.tabcontainer li").eq($(this).index()).slideDown();
+			}
+
 			}
 			return false;
 		});
